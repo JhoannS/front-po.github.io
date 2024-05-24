b5e89c04d79d266c2476a80fbf2ac7ed15b606cd
JhoannS <sebastianzamudio2004@gmail.com>
2024-05-23 21:03:58 -0500

    imporcoparacion de responsive, MAY 23 08:48 AM

--- assets/js/banderasSlider.js
+++ assets/js/banderasSlider.js
@@ -2,26 +2,26 @@ let principal2 = document.getElementById('imgBanderas')
 
 fotoBandera = new Array()
 
-fotoBandera[0] = ['../assets/img/banderas/bandera-co.webp'],
-fotoBandera[1] = ['../assets/img/banderas/bolivia.webp'],
-fotoBandera[2] = ['../assets/img/banderas/chile.webp'],
-fotoBandera[3] = ['../assets/img/banderas/costa-rica.webp'],
-fotoBandera[4] = ['../assets/img/banderas/ecuador.webp'],
-fotoBandera[5] = ['../assets/img/banderas/honduras.webp'],
-fotoBandera[6] = ['../assets/img/banderas/mexico.webp'],
-fotoBandera[7] = ['../assets/img/banderas/peru.webp'],
-fotoBandera[8] = ['../assets/img/banderas/salvador.webp'],
-fotoBandera[9] = ['../assets/img/banderas/venezuela.webp'],
-fotoBandera[10] = ['../assets/img/banderas/bandera-co.webp'],
-fotoBandera[11] = ['../assets/img/banderas/bolivia.webp'],
-fotoBandera[12] = ['../assets/img/banderas/chile.webp'],
-fotoBandera[13] = ['../assets/img/banderas/costa-rica.webp'],
-fotoBandera[14] = ['../assets/img/banderas/ecuador.webp'],
-fotoBandera[15] = ['../assets/img/banderas/honduras.webp'],
-fotoBandera[16] = ['../assets/img/banderas/mexico.webp'],
-fotoBandera[17] = ['../assets/img/banderas/peru.webp'],
-fotoBandera[18] = ['../assets/img/banderas/salvador.webp'],
-fotoBandera[19] = ['../assets/img/banderas/venezuela.webp']
+fotoBandera[0] = ['assets/img/banderas/bandera-co.webp'],
+fotoBandera[1] = ['assets/img/banderas/bolivia.webp'],
+fotoBandera[2] = ['assets/img/banderas/chile.webp'],
+fotoBandera[3] = ['assets/img/banderas/costa-rica.webp'],
+fotoBandera[4] = ['assets/img/banderas/ecuador.webp'],
+fotoBandera[5] = ['assets/img/banderas/honduras.webp'],
+fotoBandera[6] = ['assets/img/banderas/mexico.webp'],
+fotoBandera[7] = ['assets/img/banderas/peru.webp'],
+fotoBandera[8] = ['assets/img/banderas/salvador.webp'],
+fotoBandera[9] = ['assets/img/banderas/venezuela.webp'],
+fotoBandera[10] = ['assets/img/banderas/bandera-co.webp'],
+fotoBandera[11] = ['assets/img/banderas/bolivia.webp'],
+fotoBandera[12] = ['assets/img/banderas/chile.webp'],
+fotoBandera[13] = ['assets/img/banderas/costa-rica.webp'],
+fotoBandera[14] = ['assets/img/banderas/ecuador.webp'],
+fotoBandera[15] = ['assets/img/banderas/honduras.webp'],
+fotoBandera[16] = ['assets/img/banderas/mexico.webp'],
+fotoBandera[17] = ['assets/img/banderas/peru.webp'],
+fotoBandera[18] = ['assets/img/banderas/salvador.webp'],
+fotoBandera[19] = ['assets/img/banderas/venezuela.webp']
 
 if (principal2) {
     for (i = 0; i < fotoBandera.length; i++) {
--- assets/js/trabajosSlider.js
+++ assets/js/trabajosSlider.js
@@ -2,26 +2,26 @@ let principal = document.getElementById('imgTrabajo')
 
 fotoTrabajo = new Array()
 
-fotoTrabajo[0] = ['../assets/img/Recursos polimericos/1.webp'],
-fotoTrabajo[1] = ['../assets/img/Recursos polimericos/2.webp'],
-fotoTrabajo[2] = ['../assets/img/Recursos polimericos/3.webp'],
-fotoTrabajo[3] = ['../assets/img/Recursos polimericos/4.webp'],
-fotoTrabajo[4] = ['../assets/img/Recursos polimericos/5.webp'],
-fotoTrabajo[5] = ['../assets/img/Recursos polimericos/6.webp'],
-fotoTrabajo[6] = ['../assets/img/Recursos polimericos/7.webp'],
-fotoTrabajo[7] = ['../assets/img/Recursos polimericos/8.webp'],
-fotoTrabajo[8] = ['../assets/img/Recursos polimericos/9.webp'],
-fotoTrabajo[9] = ['../assets/img/Recursos polimericos/10.webp'],
-fotoTrabajo[10] = ['../assets/img/Recursos polimericos/11.webp'],
-fotoTrabajo[11] = ['../assets/img/Recursos polimericos/12.webp'],
-fotoTrabajo[12] = ['../assets/img/Recursos polimericos/13.webp'],
-fotoTrabajo[13] = ['../assets/img/Recursos polimericos/14.webp'],
-fotoTrabajo[14] = ['../assets/img/Recursos polimericos/15.webp'],
-fotoTrabajo[15] = ['../assets/img/Recursos polimericos/16.webp'],
-fotoTrabajo[16] = ['../assets/img/Recursos polimericos/17.webp'],
-fotoTrabajo[17] = ['../assets/img/Recursos polimericos/18.webp'],
-fotoTrabajo[18] = ['../assets/img/Recursos polimericos/19.webp'],
-fotoTrabajo[19] = ['../assets/img/Recursos polimericos/20.webp']
+fotoTrabajo[0] = ['assets/img/Recursos polimericos/1.webp'],
+fotoTrabajo[1] = ['assets/img/Recursos polimericos/2.webp'],
+fotoTrabajo[2] = ['assets/img/Recursos polimericos/3.webp'],
+fotoTrabajo[3] = ['assets/img/Recursos polimericos/4.webp'],
+fotoTrabajo[4] = ['assets/img/Recursos polimericos/5.webp'],
+fotoTrabajo[5] = ['assets/img/Recursos polimericos/6.webp'],
+fotoTrabajo[6] = ['assets/img/Recursos polimericos/7.webp'],
+fotoTrabajo[7] = ['assets/img/Recursos polimericos/8.webp'],
+fotoTrabajo[8] = ['assets/img/Recursos polimericos/9.webp'],
+fotoTrabajo[9] = ['assets/img/Recursos polimericos/10.webp'],
+fotoTrabajo[10] = ['assets/img/Recursos polimericos/11.webp'],
+fotoTrabajo[11] = ['assets/img/Recursos polimericos/12.webp'],
+fotoTrabajo[12] = ['assets/img/Recursos polimericos/13.webp'],
+fotoTrabajo[13] = ['assets/img/Recursos polimericos/14.webp'],
+fotoTrabajo[14] = ['assets/img/Recursos polimericos/15.webp'],
+fotoTrabajo[15] = ['assets/img/Recursos polimericos/16.webp'],
+fotoTrabajo[16] = ['assets/img/Recursos polimericos/17.webp'],
+fotoTrabajo[17] = ['assets/img/Recursos polimericos/18.webp'],
+fotoTrabajo[18] = ['assets/img/Recursos polimericos/19.webp'],
+fotoTrabajo[19] = ['assets/img/Recursos polimericos/20.webp']
 
 if (principal) {
     for (i = 0; i < fotoTrabajo.length; i++) {
